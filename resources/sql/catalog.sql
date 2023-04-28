378e7ff565fc2b52a2ef55a2cc4d2f250cfd9b4a
Veppam2 <v23ec02@ciencias.unam.mx>
2023-04-27 22:50:22 -0600

    Antes de agregar a otra migración

--- resources/sql/catalog.sql
+++ resources/sql/catalog.sql
@@ -18,3 +18,43 @@ where isbn = :isbn
 -- :name get-books :? :*
 select isbn, true as "available"
 from catalog.book;
+
+
+-- :name book-is-in-loan-table :? :1
+select loan_id from catalog.loan
+where loan_id = :loan-id;
+
+-- :name insert-loan! :! :1
+insert into catalog.loan
+(item_id, user_id, loan_date, return_date) values
+(:item-id, :user-id, current_date, current_date -interval* '2 week')
+returning *;
+
+-- :name insert-item! :! :1
+insert into catalog.item
+(item_id, user_id) values
+(:item-id, :user-id)
+returning *;
+
+-- :name delete-loan! :? :*
+delete from catalog.loan
+where item_id = :item-id and user_id = :user-id;
+
+-- :name delete-item! :? :*
+delete from catalog.item
+where item_id = :item-id;
+
+-- :name get-user-book-loans :? :*
+select * from
+catalog.loans natural join catalog.item natural join catalog.book
+where user_id = :user-id;
+
+-- :name get-number-of-total-item-books :? :1
+select count(*) from
+catalog.book natural join catalog.item
+where isbn = :isbn;
+
+-- :name get-number-of-total-lend-books :? :1
+select count(*) from
+catalog.book natural join catalog.loan
+where isbn = :isbn;
--- src/clj/sugbi/catalog/db.clj
+++ src/clj/sugbi/catalog/db.clj
@@ -14,3 +14,35 @@
   (map
    #(medley/map-keys csk/->kebab-case %)
    (search {:title (str "%" (str/lower-case title) "%")})))
+
+
+;;User asks for a book
+(defn book-in-loan [item-id]
+  (if (book-is-in-loan-table {:loan-id item-id}) true false)
+)
+
+
+(defn checkout-book [user-id book-item-id]
+  (if (book-in-loan book-item-id)
+    (
+     (insert-loan! {:item-id book-item-id
+                    :user-id user-id}
+     )
+     (delete-item! {:item-id book-item-id
+                    :user-id user-id}
+     )
+    )
+    "El ejemplar no está disponible"
+  )
+)
+
+;;Returning a book
+(defn return-book [user-id book-item-id]
+  (delete-loan! {:item-id book-item-id :user-id user-id } )
+  (insert-item! {:item-id book-item-id :user-id user-id } )
+)
+
+;;Get books that a user has
+(defn get-book-lendings [user-id]
+  (get-user-book-loans {:user-id user-id})
+)
